60c6a6fbad7523d26406fdc2eeaa30af52a6c8e7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-12 18:05:42 +0300

    ECommerceProjectWithWebAPIContext Created

---
+++ DataAccess/Concrete/Contexts/ECommerceProjectWithWebAPIContext.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Concrete.EntityFramework.Mapping;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.Contexts
+{
+    public class ECommerceProjectWithWebAPIContext : DbContext
+    {
+        public ECommerceProjectWithWebAPIContext(DbContextOptions<ECommerceProjectWithWebAPIContext> options) : base(options)
+        {
+        }
+
+        public ECommerceProjectWithWebAPIContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-C8S1J58; Initial Catalog = ECommerceProjectWithWebAPIDb; Integrated Security = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
