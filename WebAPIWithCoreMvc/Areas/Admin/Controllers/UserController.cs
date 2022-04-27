48f4708eba8b56edd90f0ceeb8d10ac876273f65
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 17:56:28 +0300

    CoreMvc Areas Admin Controllers Auth, Home & User Added

---
+++ WebAPIWithCoreMvc/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIWithCoreMvc.ApiServices.Interfaces;
+
+namespace WebAPIWithCoreMvc.Areas.Admin.Controllers
+{
+    [Authorize] // Token yok ise giriş yapamıyacak
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUserApiService _userApiService;
+
+        public UserController(IUserApiService userApiService)
+        {
+            _userApiService = userApiService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userApiService.GetListAsync();
+            return View(users);
+        }
+    }
+}
