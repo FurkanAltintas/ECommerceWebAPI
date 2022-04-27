fa3358825c52ee2ebf276e177c69f1920ebe2330
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 17:50:27 +0300

    Core DataAccess IBaseRepository Added

---
+++ Core/DataAccess/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Abstract;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess
+{
+    public interface IBaseRepository<T> where T : class, IEntity, new()
+    {
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
