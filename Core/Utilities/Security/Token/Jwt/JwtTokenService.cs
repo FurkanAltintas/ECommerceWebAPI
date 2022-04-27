0366587afcef9fcefdc13024f5eed3d6cc63d16e
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 17:52:06 +0300

    Core Utilities Security Token Jwt Added

---
+++ Core/Utilities/Security/Token/Jwt/JwtTokenService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Utilities.Security.Token.Jwt
+{
+    public class JwtTokenService : ITokenService
+    {
+        private AppSettings _appSettings;
+
+        public JwtTokenService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public AccessToken CreateToken(int userId, string userName)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.SecurityKey);
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.NameIdentifier, userId.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7), // 7 gün geçerli olucak
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return new AccessToken()
+            {
+                Token = tokenHandler.WriteToken(token),
+                Expiration = (DateTime)tokenDescriptor.Expires,
+                UserName = userName,
+                UserId = userId
+            };
+        }
+    }
+}
