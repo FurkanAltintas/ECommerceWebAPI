6a884077a2143ca59e713824ae2e7d2086719801
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:01:04 +0300

    DataAccess Concrete EntityFramework EfUserDal Added

--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -1,10 +1,11 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using DataAccess.Concrete.Contexts;
 using Entities.Concrete;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfUserDal :EfBaseRepository<User, ECommerceProjectWithWebAPIContext>, IUserDal
+    public class EfUserDal : EfBaseRepository<User, ECommerceProjectWithWebAPIContext>, IUserDal
     {
     }
 }
