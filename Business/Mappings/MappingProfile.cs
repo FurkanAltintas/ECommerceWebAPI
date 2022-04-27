536ea57f837a23b8f0075ca143b11512f054cfb0
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 17:49:03 +0300

    AutoMapper MappingProfile Added

---
+++ Business/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Entities.Concrete;
+using Entities.Dtos.UserDtos;
+
+namespace Business.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDetailDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, UserAddDto>().ReverseMap();
+            CreateMap<User, UserUpdateDto>().ReverseMap();
+            CreateMap<UserDto, UserUpdateDto>().ReverseMap();
+
+            // TSource, TDestination
+        }
+    }
+}
