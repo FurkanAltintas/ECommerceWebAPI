975f6b06e2e945aaa1a635b011e6d473c826a4c3
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:04:58 +0300

    WebAPI

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,34 +1,56 @@
+using AutoMapper;
+using Business.Abstract;
+using Business.Concrete;
+using Business.Mappings;
+using Core.Extensions;
+using Core.Utilities.Security.Token;
+using Core.Utilities.Security.Token.Jwt;
 using DataAccess.Abstract;
 using DataAccess.Concrete.Contexts;
 using DataAccess.Concrete.EntityFramework;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ECommerceProjectWithWebAPIContext>();
 
+#region DI
 builder.Services.AddTransient<IUserDal, EfUserDal>();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<ITokenService, JwtTokenService>();
+builder.Services.AddTransient<IAuthService, AuthService>();
+#endregion
 
-// Add services to the container.
+#region AutoMapper
+var mapperConfiguration = new MapperConfiguration(configuration =>
+{
+    configuration.AddProfile(new MappingProfile());
+});
+var mapper = mapperConfiguration.CreateMapper();
+builder.Services.AddSingleton(mapper);
+#endregion
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCustomSwagger();
+builder.Services.AddCustomJwtToken(builder.Configuration);
+
+
+// --------------------------------------------------------------------------------- //
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseCustomSwagger();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseRouting();
+app.UseAuthentication(); // Kimlik doðrulama
+app.UseAuthorization();  // Yetkilendirme
 
 app.MapControllers();
 
