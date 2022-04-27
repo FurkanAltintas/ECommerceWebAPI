364f53ca986d88452747124be0a4a92bf9f5e7c3
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 18:02:42 +0300

    CoreMvc ViewModels UserAdd & UserUpdate ViewModel Added

---
+++ WebAPIWithCoreMvc/ViewModels/UserUpdateViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIWithCoreMvc.ViewModels
+{
+    public class UserUpdateViewModel
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public int GenderId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+    }
+}
