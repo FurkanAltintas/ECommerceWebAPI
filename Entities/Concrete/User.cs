9e475a4941e206f50edad47f80e64496437ec101
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 17:58:00 +0300

    Include Core

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Concrete.BaseEntities;
+﻿using Core.Entities.Concrete;
 
 namespace Entities.Concrete
 {
@@ -12,5 +12,8 @@ namespace Entities.Concrete
         public bool Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Token { get; set; }
+        public DateTime? TokenExpireDate { get; set; }
     }
 }
