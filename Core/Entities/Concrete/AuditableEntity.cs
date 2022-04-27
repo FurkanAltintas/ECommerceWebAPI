197ec0c9693771067107fba5852f87c41d22ebf7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-04-27 17:51:15 +0300

    Core Entities Concrete Entity Added

---
+++ Core/Entities/Concrete/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Abstract;
+
+namespace Core.Entities.Concrete
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+        public int CreatedUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedUserId { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
